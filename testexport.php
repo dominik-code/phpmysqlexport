3c9d49145e35c29bf3b0ba54b13b230129ce3a7e
dominik-code <git@dominik-schmitt.eu>
2019-04-22 21:08:06 +0200

    Added debugging

--- testexport.php
+++ testexport.php
@@ -4,7 +4,7 @@
  * the final version should allow the export of specific sql results to simple sql files (or even better compressed sql files)
  *
  */
-
+$start = microtime(true);
 require_once 'libs/MySQLExport.php';
 
 require_once 'configuration.php';
@@ -12,15 +12,17 @@ require_once 'configuration.php';
 $export = new MySQLExport(HOST, PORT, USERNAME, PASSWORD, DATABASE);
 
 $export->testConnection();
-
+var_dump(microtime(true) - $start);
 $export->setExportFilename("testexport.sql");
 $export->setExportPath("");
-$export->setExportSQL("SELECT * FROM `errorlog` WHERE id > 250000");
+$export->setExportSQL("SELECT * FROM `zuege2` WHERE datum='2018-06-12' ");
+var_dump(microtime(true) - $start);
 $export->setMaxrowsperinsert(500);
 $export->setMaxrowsperloop(100000);
 var_dump($export->doExport());
-var_dump($export->testInsertHead());
+var_dump(microtime(true) - $start);
 
 var_dump(memory_get_peak_usage());
 
 var_dump($export->gzCompressFile("testexport.sql"));
+var_dump(microtime(true) - $start);
